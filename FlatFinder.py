e839484decca5bcf69be1d1ffe547aa2807033fa
Aldahunter <alex.d.hunt@icloud.com>
2021-09-24 00:04:16 +0100

    FF001 Added Google Maps API Key

--- FlatFinder.py
+++ FlatFinder.py
@@ -1,7 +1,20 @@
+import json
+
 class FlatFinder:
 
+    API_KEY=None
+    CREDENTIALS_JSON="credentials.json"
+
     def __init__(self) -> None:
+        self.get_api_key()
         quit()
+    
+    def _load_credentials(self):
+        with open(self.CREDENTIALS_JSON) as json_file:
+            return json.load(json_file)
+
+    def get_api_key(self):
+        self.API_KEY = self._load_credentials()['API_KEY']
 
 if __name__ == "__main__":
     FlatFinder()
