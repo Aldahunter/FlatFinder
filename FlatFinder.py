f868e7a4b30bcd81aab42a48ba52185707fe10ec
Aldahunter <alex.d.hunt@icloud.com>
2021-09-24 00:08:26 +0100

    FF002 Added database for storage

--- FlatFinder.py
+++ FlatFinder.py
@@ -1,12 +1,15 @@
 import json
+import sqlite3 as sl
 
 class FlatFinder:
 
     API_KEY=None
     CREDENTIALS_JSON="credentials.json"
+    DATABASE_NAME="FlatFinder.db"
 
     def __init__(self) -> None:
         self.get_api_key()
+        DB = sl.connect(self.DATABASE_NAME)
         quit()
     
     def _load_credentials(self):
